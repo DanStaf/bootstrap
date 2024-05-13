36b676776695176a01fc01ba40bbeeab45290050
DanStaf <danstaf@mail.ru>
2024-05-13 18:17:29 +0300

    'contacts' Controller updated

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home.html')
 
+
 def contacts(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        print('INPUT DATA: ', name)
+
     return render(request, 'contacts.html')
+
+
