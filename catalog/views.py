bde31b81305f7fcb00bb3c7a60870b92173647b6
DanStaf <danstaf@mail.ru>
2024-06-03 21:43:07 +0300

    new logic for Articles views added

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from catalog.models import Product, Contact, Article
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from pytils.translit import slugify
 
 # Create your views here.
 
@@ -69,9 +70,26 @@ def get_object_or_None(My_Model, pk):
 class ArticleListView(ListView):
     model = Article
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class ArticleDetailView(DetailView):
     model = Article
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_qty += 1
+        self.object.save()
+
+        if self.object.views_qty == 100:
+            print(f'''C O N G R A T U L A T I O N S ! ! ! ! !
+the Article "{self.object.title}" reached 100 views!
+''')
+
+        return self.object
 
 
 class ArticleCreateView(CreateView):
@@ -79,11 +97,30 @@ class ArticleCreateView(CreateView):
     fields = ('title', 'content', 'photo',)
     success_url = reverse_lazy('catalog:blog')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_article = form.save()
+            new_article.slug = slugify(new_article.title)
+            new_article.save()
+
+        return super().form_valid(form)
+
 
 class ArticleUpdateView(UpdateView):
     model = Article
-    fields = ('title', 'content', 'photo',)
-    success_url = reverse_lazy('catalog:blog')
+    fields = ('title', 'content', 'photo', 'is_published')
+    # success_url = reverse_lazy('catalog:blog')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_article = form.save()
+            new_article.slug = slugify(new_article.title)
+            new_article.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('catalog:view_article', args=[self.kwargs.get('pk')])
 
 
 class ArticleDeleteView(DeleteView):
