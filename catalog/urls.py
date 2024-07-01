5de208b9e6b37ef4da81478711204d6deae1097c
DanStaf <danstaf@mail.ru>
2024-07-01 17:03:40 +0300

    Redis cache added. Controller for one product cached

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 from catalog.views import ContactListView, product_publish, ProductDescriptionUpdateView, ProductCategoryUpdateView
 from catalog.views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
+from config.settings import CACHE_ENABLED
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', ContactListView.as_view(), name='contacts'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
+
+    path('product/<int:pk>',
+         cache_page(60)(ProductDetailView.as_view()) if CACHE_ENABLED else ProductDetailView.as_view(),
+         name='product'
+         ),
+
     path('product/create', ProductCreateView.as_view(), name='create_product'),
     path('product/update/<int:pk>', ProductUpdateView.as_view(), name='update_product'),
     path('product/delete/<int:pk>', ProductDeleteView.as_view(), name='delete_product'),
